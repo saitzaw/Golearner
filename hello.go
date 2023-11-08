c45b6512df3dc2f098709261d63e3e4b24424019
Sai Thiha Zaw <sai.t.zaw@frontiir.net>
2023-11-08 14:22:40 +0630

    add the hello world for go

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 26
+	var name string = "John Doe"
+	var greeting string = "Happy Birthday!"
+
+	fmt.Printf("%s %d years, %s \n", greeting, age, name)
+}
