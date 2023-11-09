f12e6094d65cebfadc3203f017d5a06180801eec
Sai Thiha Zaw <sai.t.zaw@frontiir.net>
2023-11-09 11:08:27 +0630

    add the simple go  login

--- SimpleLogin.go
+++ SimpleLogin.go
@@ -1,27 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func SimpleLogin() {
+type EmptyStringError struct {
+	Message string
+}
+
+func (e *EmptyStringError) Error() string {
+	return e.Message
+}
+
+func simpleLogin() error {
+
 	// User input - User name
 	fmt.Println("Enter the user name: ")
 	var name string
 	fmt.Scanln(&name)
+
+	if len(name) == 0 {
+		return &EmptyStringError{"Empty String Error!"}
+	}
 
 	// User input - Password
 	fmt.Println("Enter the password: ")
 	var password string
 	fmt.Scanln(&password)
 
-	// Conditional check here
+	if len(password) == 0 {
+		return &EmptyStringError{"Empty String Error!"}
+	}
 
+	// Conditional check here
 	if name == "admin" && password == "password123" {
 		fmt.Println("Login successful")
+		return nil
 	} else {
 		fmt.Println("Login fail")
+		return errors.New("Log in Fail")
 	}
 }
 
 func main() {
-	SimpleLogin()
+	simpleLogin()
 }
